970193442702dab6c511c3ce9f58a6e4b75ebe27
mayank <mayank.upmanyu@galaxyweblinks.in>
2022-08-03 13:10:21 +0530

    nothing changes

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -26,31 +26,19 @@ class PermissionsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $permissions = Permission::all();
+        // $permissions = Permission::all();
 
-        try {
-            foreach($permissions as $permission) {
-                Gate::define($permission->slug, function ($user) use ($permission) {
-                    return $user->hasPermissionTo($permission);
-                });
-            }
-            // Permission::get()->map(function ($permission) {
-            //     Gate::define($permission->slug, function ($user) use ($permission) {
-            //         return $user->hasPermissionTo($permission);
-            //     });
-            // });
-        } catch (\Exception $e) {
-            report($e);
-            return false;
-        }
+        // try {
+        //     foreach($permissions as $permission) {
+        //         Gate::define($permission->slug, function ($user) use ($permission) {
+        //             return $user->hasPermissionTo($permission);
+        //         });
+        //     }
+            
+        // } catch (\Exception $e) {
+        //     report($e);
+        //     return false;
+        // }
 
-        // //Blade directives
-        // Blade::directive('role', function ($role) {
-        //      return "if(auth()->check() && auth()->user()->hasRole({$role})) :"; //return this if statement inside php tag
-        // });
-
-        // Blade::directive('endrole', function ($role) {
-        //      return "endif;"; //return this endif statement inside php tag
-        // });
     }
 }
